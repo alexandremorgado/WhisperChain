9ab58d0c6b8f1a8b698ea8eb72b606cf3a3ed461
Alexandre Morgado <alexandremorgado@gmail.com>
2021-12-21 07:42:33 -0300

    Initial Commit

---
+++ Shared/WhisperChainApp.swift
@@ -0,0 +1,17 @@
+//
+//  WhisperChainApp.swift
+//  Shared
+//
+//  Created by Alexandre Morgado on 21/12/21.
+//
+
+import SwiftUI
+
+@main
+struct WhisperChainApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
